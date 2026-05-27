508a57e5a8ee14be4c011ca0d06da417d841b669
aurelio <aurelioserafim2807@gmail.com>
2026-05-26 23:04:27 -0300

    feature engineering for ecs

--- scripts/main.py
+++ scripts/main.py
@@ -1,8 +1,8 @@
 import os
 import subprocess
 
-PATH_CLEANING = "cleaning.py"
-PATH_SELECTION = "selection.py"
+PATH_CLEANING = "scripts/cleaning.py"
+PATH_SELECTION = "scripts/selection.py"
 
 if __name__ == "__main__":
     p1 = subprocess.Popen(["python", PATH_CLEANING])
